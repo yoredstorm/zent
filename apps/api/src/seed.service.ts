4a690f67fb009ca429cc50e7d4b773c962ee670f
yoredstorm <yoredstorm@users.noreply.github.com>
2026-06-28 23:31:16 -0500

    Fix: import path en seed.service.ts

--- apps/api/src/seed.service.ts
+++ apps/api/src/seed.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
